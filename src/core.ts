764ce2b305b6e0f155984ecc6e5e535447d07283
Tolu <tolugbesan@gmail.com>
2021-10-08 22:06:49 +0100

    🔨Add logic to detect when a URL appears in app source code

--- src/core.ts
+++ src/core.ts
@@ -3,6 +3,21 @@ import * as fs from 'fs-extra';
 import { TextDecoder, TextEncoder } from 'util';
 import * as recast from 'recast';
 import { v4 as uuid4 } from 'uuid';
+import { visit } from 'ast-types';
+
+// ========================================================= LOGGING =========================================================
+
+// const winston = require('winston');
+
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//     new winston.transports.File({ filename: 'combined.log' }),
+//   ],
+// });
 
 // ========================================================= MYSTIQUE CLASS =========================================================
 
@@ -38,6 +53,7 @@ export class Mystique {
         .then(async config => {
             this.config = config; // ISSUE: returns undefined when it encounters bad JSON in config file
             this.monitorFrontend();
+            this.createServer();
         })
         .catch(err => {
             console.error(err);
@@ -46,10 +62,11 @@ export class Mystique {
 
     private getNewConfiguration(): string {
         return `{
-    "mistiqueID": "${uuid4()}",
+    "mystiqueID": "${uuid4()}",
     "settings": {
         "version": "1.0.0",
         "mode": "auto",
+        "rootDir": "${Helper.correctFilePathForJSON(this.rootDir)}",
         "target": ["${Helper.correctFilePathForJSON(this.activeFileName)}"],
         "preserve": true
     }
@@ -61,6 +78,7 @@ export class Mystique {
         const path = this.rootDir + '/mistique.json';
         return fs.pathExists(path)
         .then(async val => { 
+            let config = undefined;
             let preserve = true;    
             if (val) {
                 // Do not update preserved configuration
@@ -68,9 +86,8 @@ export class Mystique {
                 let data = await vscode.workspace.fs.readFile(uri);
                 let str = Helper.decodeText(data);
                 try {
-                    let config = JSON.parse(str);
+                    config = JSON.parse(str);
                     preserve = config.settings.preserve;
-                    return config;
                 } catch (err) {
                     console.error(err);
                     vscode.window.showErrorMessage('Bad JSON in mystique.json');                
@@ -84,17 +101,19 @@ export class Mystique {
                 .then(() => {
                     config = JSON.parse(config);
                     vscode.window.showInformationMessage('Configuration set up completed');
-                    return config;
                 }, err => {
                     vscode.window.showErrorMessage('Configuration set up failed');
                     console.log(err);
                 });
             }
+            return config;
         });
     };
 
     private async createServer() {
-
+        let template = `
+`;
+        fs.createFile(`./.mocks/${this.config.mystiqueID}.js`);
     };
 
     public async updateServer() {
@@ -126,20 +145,37 @@ export class Mystique {
         let uri = vscode.Uri.file(sourcePath);
         vscode.workspace.openTextDocument(uri)
         .then(document => {
+            // BUG: Detects two changes for one change at the start up of the extension?
             vscode.workspace.onDidChangeTextDocument(async event => {
                 if (event.document === document) {
+                    let sourceID = Helper.correctFilePathForJSON(document.uri.fsPath); // Identify by Path
                     console.log(`Detected changes in: ${sourcePath}`);
+                    // debugger;
                     // TODO: Look for a more efficient way to watch files 
                     // without having to store the whole document everytime
                     //
                     // NOTE: It might be better to wait for a save before trying to parse
                     try {
-                        tree = await Mystique.parseSource(document.getText()); 
-                        this.appForest.push(tree);
-                        console.log('Tree: ');
-                        console.log(tree);
+                        tree = await Mystique.parseSource(document.getText());
+                        const find = this.appForest.filter(treeObj => {
+                            return treeObj.sourceID === sourceID;
+                        }).length;
+
+                        if (find === 1) {
+                            let i = this.appForest.findIndex(treeObj => {
+                                return treeObj.sourceID === sourceID;
+                            });
+                            this.appForest[i].tree = tree;
+                        } else if (find === 0) {
+                            this.appForest.push({sourceID: `${sourceID}`, tree: tree}); 
+                        } else {
+                            throw new Error('More than one source ID was found in instance variable --> appForest');
+                        }
+                                            
+                        this.shapeshiftAPI(this.appForest);
                         
-                    } catch (err) {                        
+                    } catch (err) {    
+                        console.error(err); // TODO: Catch other errors          
                         // Ignore bad JS
                     }
                 }         
@@ -151,8 +187,93 @@ export class Mystique {
             console.error(err);
         });
     }
+    
+    // Primitive implementation of a source code to API mocking approach using a set of bare-bones heuristics, for now 
+    private async shapeshiftAPI(forest: any[]) {
+        console.log('shapeshifting API');
 
-    // Primitive implementation of an API mocking approach using a set of bare-bones heuristics 
+        // Hardcoded heuristics to detect for APIs and their data
+        //
+        // NOTE: An ANN should replace the logic here as soon as possible
+
+        let getFetch = (tree: any) => {
+            let count = 0;
+            visit(tree, {
+                visitCallExpression: function (path) {
+                    
+                    if (path.value.callee.type === 'Identifier' && path.value.callee.name === 'fetch') {
+                        console.log('Detected use of Fetch');
+                        ++count;
+                        // console.log(path.value);
+                    } 
+                    this.traverse(path);
+                }
+            });
+            console.log(`Fetch was used ${count} times`);
+        };
+
+         let getAxios = (tree: any) => {
+            let count = 0;
+            visit(tree, {
+                visitCallExpression: function (path) {
+                    try {
+                        if (path.value.callee.object.name === 'axios') {
+                            console.log('Detected use of Axios');
+                            ++count;
+                            // console.log(path.value);
+                        }                        
+                    } catch (err) {
+                        // Do nothing
+                    }                    
+                    this.traverse(path);
+                }
+            });
+            console.log(`Axios was used ${count} times`);
+        };
+
+         let getXMLHttp = (tree: any) => {
+            let count = 0;
+            visit(tree, {
+                visitNewExpression: function (path) {                        
+                    if (path.value.callee.name === 'XMLHttpRequest') {
+                        console.log('Detected use of XMLHttpRequests');
+                        ++count;
+                        // console.log(path.value);     
+                    }                  
+                    this.traverse(path);
+                }
+                // visitVariableDeclaration: function (path) {                        
+                //     path.value.declarations.forEach((declaration: any) => {
+                //         debugger;
+                //         if (declaration.init.callee.name === 'XMLHttpRequest') {
+                //             console.log('Detected use of XMLHttpRequests');
+                //             ++count;
+                //             // console.log(path.value);
+                //         }
+                //     });                         
+                //     // debugger;
+                //     this.traverse(path);
+                // }
+            });
+            console.log(`XMLHttpRequest was used ${count} times`);
+        };
+
+         let getHTMLForm = (tree: any) => {
+
+         };
+
+        forest.forEach(tree => {
+
+            getFetch(tree);
+            getAxios(tree);
+            getXMLHttp(tree);
+        });
+        console.log('Done.');
+        console.log('------------------------------------------');
+        console.log('==========================================');
+    }
+
+    // Provide data for mocked entities 
     private static async mock() {
 
     }
